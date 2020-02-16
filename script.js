2188d0177768b6e80eb781468bd58e54cd9d0fc9
LucasNemeth <l_orion_n@hotmail.com>
2020-02-16 13:16:08 -0500

    got a function to GET the monsters ready

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const myApp={}
 
-myApp.baseUrl=`http://www.dnd5eapi.co/api/`;
+// myApp.baseUrl=`http://www.dnd5eapi.co/api/`;
 
+myApp.getMonster = () => {
+    $.ajax({
+        url:'http://www.dnd5eapi.co/api/monsters',
+        method:'GET',
+        dataType:'json',
+        data:{
+            format:'json',
+            q:'goblin'
+        }
+        }).then(function(result){
+            console.log('any monsters here?',result)
+    })
+}
 
+myApp.init = () => {
+    myApp.getMonster();
+}
 
 $(function() {
-  
+    myApp.init();
 });
