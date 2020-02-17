49b150bd45a47ddc4e6d6d0a04df28bb6dc14b6c
LucasNemeth <l_orion_n@hotmail.com>
2020-02-17 10:18:47 -0500

    added a bit of stats to the JS

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const myApp={}
+const myApp={};
 
 // myApp.baseUrl=`http://www.dnd5eapi.co/api/`;
+myApp.displayMonster = (monster) => {
+    $('.name-1').html(monster.name);
+    $('.monster').html('');
+    const hitPoints = `<li><p> Hit Points: ${monster.hit_points}</p></li>`
+    const strength = `<li><p> Strength: ${monster.strength}</p></li>`
+    const con = `<li><p> Constitution: ${monster.constitution}</p></li>`
+    const dex = `<li><p> Dexterity: ${monster.dexterity}</p></li>`
+    const int = `<li><p> Intelligence: ${monster.intelligence}</p></li>`
+    const wis = `<li><p> Wisdom: ${monster.wisdom}</p></li>`
+    const cha = `<li><p> Charisma: ${monster.charisma}</p></li>`
+    $('.monster').append(hitPoints, type, size, strength, con, int, wis, cha);
+
+    //appending that HTML
+};
+
 
 myApp.getMonster = () => {
     $.ajax({
