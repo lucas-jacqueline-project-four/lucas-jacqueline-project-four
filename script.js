bc422432832bf9bc773160febe7ba7ca033afb81
Jacqui Nosal <jacqueline.dchow@gmail.com>
2020-02-17 10:39:02 -0500

    added on click, looking for owlbear

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 const myApp={};
 
 // myApp.baseUrl=`http://www.dnd5eapi.co/api/`;
-myApp.displayMonster = (monster) => {
-    $('.name-1').html(monster.name);
-    $('.monster').html('');
-    const hitPoints = `<li><p> Hit Points: ${monster.hit_points}</p></li>`
-    const strength = `<li><p> Strength: ${monster.strength}</p></li>`
-    const con = `<li><p> Constitution: ${monster.constitution}</p></li>`
-    const dex = `<li><p> Dexterity: ${monster.dexterity}</p></li>`
-    const int = `<li><p> Intelligence: ${monster.intelligence}</p></li>`
-    const wis = `<li><p> Wisdom: ${monster.wisdom}</p></li>`
-    const cha = `<li><p> Charisma: ${monster.charisma}</p></li>`
-    $('.monster').append(hitPoints, type, size, strength, con, int, wis, cha);
 
-    //appending that HTML
-};
+myApp.getOwlbear = () => {
 
-
-myApp.getMonster = () => {
     $.ajax({
-        url:'http://www.dnd5eapi.co/api/monsters',
+        url:'http://www.dnd5eapi.co/api/monsters/owlbear',
         method:'GET',
         dataType:'json',
         data:{
@@ -31,10 +17,30 @@ myApp.getMonster = () => {
     })
 }
 
+myApp.displayMonster = (monster) => {
+    $('.name1').html(monster.name);
+    const hitPoints = `<li><p> Hit Points: ${monster.hit_points}</p></li>`
+    const strength = `<li><p> Strength: ${monster.strength}</p></li>`
+    const con = `<li><p> Constitution: ${monster.constitution}</p></li>`
+    const dex = `<li><p> Dexterity: ${monster.dexterity}</p></li>`
+    const int = `<li><p> Intelligence: ${monster.intelligence}</p></li>`
+    const wis = `<li><p> Wisdom: ${monster.wisdom}</p></li>`
+    const cha = `<li><p> Charisma: ${monster.charisma}</p></li>`
+    $('.monster').append(hitPoints, strength, con, dex, int, wis, cha);
+
+    //appending that HTML
+};
+
 myApp.init = () => {
-    myApp.getMonster();
+    
 }
 
 $(function() {
     myApp.init();
+
+    $('#stats-1').on('click', '#owlbear', function(){
+        myApp.getOwlbear();
+        myApp.displayMonster();
+        console.log("Are we working?");
+    });
 });
