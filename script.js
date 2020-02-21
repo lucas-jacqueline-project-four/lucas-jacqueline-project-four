be55c74f554406374956afd533c4ac0d4ca6604f
Jacqui Nosal <jacqueline.dchow@gmail.com>
2020-02-21 00:30:10 -0500

    best practices and requirements sweep

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const myApp={};
+// Credit to the Dungeons and Dragons API: http://www.dnd5eapi.co/api/
 
-// myApp.baseUrl=`http://www.dnd5eapi.co/api/`;
+const app = {};
 
-myApp.getOwlbear = () => {
+// Getting the stats for each monster from the API
+
+app.getOwlbear = () => {
     const monsterPromise = $.ajax({
         url: 'http://proxy.hackeryou.com',
         dataType: 'json',
@@ -11,14 +13,12 @@ myApp.getOwlbear = () => {
             reqUrl: `http://dnd5eapi.co/api/monsters/owlbear`,
         }
     }).then((data) => {
-        console.log(data);
-        // return data;
-        myApp.displayMonster(data);
+        app.displayMonster(data);
     });
-    return monsterPromise
+    return monsterPromise;
 }
 
-myApp.getGoblin = () => {
+app.getGoblin = () => {
     const monsterPromise = $.ajax({
         url: 'http://proxy.hackeryou.com',
         dataType: 'json',
@@ -27,14 +27,12 @@ myApp.getGoblin = () => {
             reqUrl: `http://dnd5eapi.co/api/monsters/goblin`,
         }
     }).then((data) => {
-        console.log(data);
-        // return data;
-        myApp.displayMonster(data);
+        app.displayMonster(data);
     });
-    return monsterPromise
+    return monsterPromise;
 }
 
-myApp.getOrc = () => {
+app.getOrc = () => {
     const monsterPromise = $.ajax({
         url: 'http://proxy.hackeryou.com',
         dataType: 'json',
@@ -43,14 +41,12 @@ myApp.getOrc = () => {
             reqUrl: `http://dnd5eapi.co/api/monsters/orc`,
         }
     }).then((data) => {
-        console.log(data);
-        // return data;
-        myApp.displayMonster(data);
+        app.displayMonster(data);
     });
-    return monsterPromise
+    return monsterPromise;
 }
 
-myApp.getSahuagin = () => {
+app.getSahuagin = () => {
     const monsterPromise = $.ajax({
         url: 'http://proxy.hackeryou.com',
         dataType: 'json',
@@ -59,14 +55,12 @@ myApp.getSahuagin = () => {
             reqUrl: `http://dnd5eapi.co/api/monsters/sahuagin`,
         }
     }).then((data) => {
-        console.log(data);
-        // return data;
-        myApp.displayMonster(data);
+        app.displayMonster(data);
     });
-    return monsterPromise
+    return monsterPromise;
 }
 
-myApp.getTroll = () => {
+app.getTroll = () => {
     const monsterPromise = $.ajax({
         url: 'http://proxy.hackeryou.com',
         dataType: 'json',
@@ -75,14 +69,12 @@ myApp.getTroll = () => {
             reqUrl: `http://dnd5eapi.co/api/monsters/troll`,
         }
     }).then((data) => {
-        console.log(data);
-        // return data;
-        myApp.displayMonster(data);
+        app.displayMonster(data);
     });
-    return monsterPromise
+    return monsterPromise;
 }
 
-myApp.getGelatinousCube = () => {
+app.getGelatinousCube = () => {
     const monsterPromise = $.ajax({
         url: 'http://proxy.hackeryou.com',
         dataType: 'json',
@@ -91,21 +83,21 @@ myApp.getGelatinousCube = () => {
             reqUrl: `http://dnd5eapi.co/api/monsters/gelatinous-cube`,
         }
     }).then((data) => {
-        console.log(data);
-        // return data;
-        myApp.displayMonster(data);;
+        app.displayMonster(data);;
     });
-    return monsterPromise
+    return monsterPromise;
 }
 
-myApp.displayMonster = (monster) => {
-    // const hitPoints = `<li><p> Hit Points: ${monster.hit_points}</p></li>`
-    const strength = `<li><p> Strength: ${monster.strength}</p></li>`
-    const con = `<li><p> Constitution: ${monster.constitution}</p></li>`
-    const dex = `<li><p> Dexterity: ${monster.dexterity}</p></li>`
-    const int = `<li><p> Intelligence: ${monster.intelligence}</p></li>`
-    const wis = `<li><p> Wisdom: ${monster.wisdom}</p></li>`
-    const cha = `<li><p> Charisma: ${monster.charisma}</p></li>`
+
+// Updating the HTML to display the monster stats and title on the page
+
+app.displayMonster = (monster) => {
+    const strength = `<li><p> Strength: ${monster.strength}</p></li>`;
+    const con = `<li><p> Constitution: ${monster.constitution}</p></li>`;
+    const dex = `<li><p> Dexterity: ${monster.dexterity}</p></li>`;
+    const int = `<li><p> Intelligence: ${monster.intelligence}</p></li>`;
+    const wis = `<li><p> Wisdom: ${monster.wisdom}</p></li>`;
+    const cha = `<li><p> Charisma: ${monster.charisma}</p></li>`;
 
     $('#select-monster-title').text(monster.name);
 
@@ -114,23 +106,24 @@ myApp.displayMonster = (monster) => {
     $('#monster-stat-box').replaceWith( strength, con, dex, int, wis, cha);
 };
 
-myApp.displayEnemy = (monster) => {
-    // const hitPoints = `<li><p> Hit Points: ${monster.hit_points}</p></li>`
-    const strength = `<li><p> Strength: ${monster.strength}</p></li>`
-    const con = `<li><p> Constitution: ${monster.constitution}</p></li>`
-    const dex = `<li><p> Dexterity: ${monster.dexterity}</p></li>`
-    const int = `<li><p> Intelligence: ${monster.intelligence}</p></li>`
-    const wis = `<li><p> Wisdom: ${monster.wisdom}</p></li>`
-    const cha = `<li><p> Charisma: ${monster.charisma}</p></li>`
+app.displayEnemy = (monster) => {
+    const strength = `<li><p> Strength: ${monster.strength}</p></li>`;
+    const con = `<li><p> Constitution: ${monster.constitution}</p></li>`;
+    const dex = `<li><p> Dexterity: ${monster.dexterity}</p></li>`;
+    const int = `<li><p> Intelligence: ${monster.intelligence}</p></li>`;
+    const wis = `<li><p> Wisdom: ${monster.wisdom}</p></li>`;
+    const cha = `<li><p> Charisma: ${monster.charisma}</p></li>`;
 
     $('#enemy-monster-title').text(monster.name);
 
     $('#enemy-stat-box').replaceWith(strength, con, dex, int, wis, cha);
 };
 
-myApp.getRandomMonster = () => {
+
+// Generating a random enemy monster
+
+app.getRandomMonster = () => {
     const listOfRandomOpponents = ['goblin', 'owlbear', 'orc', 'troll', 'sahuagin', 'gelatinous cube'];
-    // const listOfRandomOpponents = [ 'sahuagin', 'gelatinous cube'];
     const random = Math.floor(Math.random() * listOfRandomOpponents.length);
     
     if (listOfRandomOpponents[random] === 'goblin') {
@@ -142,18 +135,14 @@ myApp.getRandomMonster = () => {
                 reqUrl: `http://dnd5eapi.co/api/monsters/goblin`,
             }
         }).then((data) => {
-            console.log(data);
-            // return data;
-            myApp.displayEnemy(data);
-            $('#enemy-img').attr('src', './assets/gobbo.png');
+            app.displayEnemy(data);
+            $('#enemy-img').attr('src', './assets/gobbo.png').attr('alt', 'Goblin');
             enemyHP = 40;
             $('#enemy-hp').append(`<p class="hp">HP: ${enemyHP}/40</p>`);
-            $('body').on('click', '#attack', function () {
+            $('body').on('click', '#attack', function() {
                 enemyHP = enemyHP - 5;
                 $('#enemy-hp').replaceWith(`<p class="hp" id="enemy-hp">HP: ${enemyHP}/40`);
-                console.log(enemyHP);
-                console.log($('#enemy-hp'));
-                myApp.checkWinner();
+                app.checkWinner();
             });
         });
         return enemyMonsterPromise;
@@ -166,25 +155,18 @@ myApp.getRandomMonster = () => {
                 reqUrl: `http://dnd5eapi.co/api/monsters/owlbear`,
             }
         }).then((data) => {
-            console.log(data);
-            // return data;
-            myApp.displayEnemy(data);
-            $('#enemy-img').attr('src', './assets/Owlbear2.png');
+            app.displayEnemy(data);
+            $('#enemy-img').attr('src', './assets/Owlbear2.png').attr('alt', 'Owlbear');
             enemyHP = 59;
             $('#enemy-hp').append(`<p class="hp">HP: ${enemyHP}/59</p>`);
-            $('body').on('click', '#attack', function () {
+            $('body').on('click', '#attack', function() {
                 enemyHP = enemyHP - 5;
                 $('#enemy-hp').replaceWith(`<p class="hp" id="enemy-hp">HP: ${enemyHP}/59`);
-                console.log(enemyHP);
-                console.log($('#enemy-hp'));
-                myApp.checkWinner();
+                app.checkWinner();
             });
         });
         return enemyMonsterPromise;
     } else if (listOfRandomOpponents[random] === 'orc') {
-        // const enemyAttackedHP
-        // const attackedOrcHP = enemyOrcHP;
-        // return attackedOrcHP;
         const enemyMonsterPromise = $.ajax({
             url: 'http://proxy.hackeryou.com',
             dataType: 'json',
@@ -193,18 +175,14 @@ myApp.getRandomMonster = () => {
                 reqUrl: `http://dnd5eapi.co/api/monsters/orc`,
             }
         }).then((data) => {
-            console.log(data);
-            // return data;
-            myApp.displayEnemy(data);
-            $('#enemy-img').attr('src', './assets/orc.png');
+            app.displayEnemy(data);
+            $('#enemy-img').attr('src', './assets/orc.png').attr('alt', 'Orc');
             enemyHP =  50;
             $('#enemy-hp').append(`<p class="hp">HP: ${enemyHP}/50</p>`); 
-            $('body').on('click', '#attack', function(){ 
+            $('body').on('click', '#attack', function() { 
                 enemyHP = enemyHP - 5;
                 $('#enemy-hp').replaceWith(`<p class="hp" id="enemy-hp">HP: ${enemyHP}/50`);
-                console.log(enemyHP);
-                console.log($('#enemy-hp'));
-                myApp.checkWinner();
+                app.checkWinner();
             });
         });
         return enemyMonsterPromise;
@@ -217,18 +195,14 @@ myApp.getRandomMonster = () => {
                 reqUrl: `http://dnd5eapi.co/api/monsters/troll`,
             }
         }).then((data) => {
-            console.log(data);
-            // return data;
-            myApp.displayEnemy(data);
-            $('#enemy-img').attr('src', './assets/O-Mad-Troll-Monster-620-Wide-with-margins.png');
+            app.displayEnemy(data);
+            $('#enemy-img').attr('src', './assets/O-Mad-Troll-Monster-620-Wide-with-margins.png').attr('alt', 'Troll');
             enemyHP = 84;
             $('#enemy-hp').append(`<p class="hp">HP: ${enemyHP}/84</p>`);
-            $('body').on('click', '#attack', function () {
+            $('body').on('click', '#attack', function() {
                 enemyHP = enemyHP - 5;
                 $('#enemy-hp').replaceWith(`<p class="hp" id="enemy-hp">HP: ${enemyHP}/84`);
-                console.log(enemyHP);
-                console.log($('#enemy-hp'));
-                myApp.checkWinner();
+                app.checkWinner();
             });
         });
         return enemyMonsterPromise;
@@ -241,18 +215,14 @@ myApp.getRandomMonster = () => {
                 reqUrl: `http://dnd5eapi.co/api/monsters/sahuagin`,
             }
         }).then((data) => {
-            console.log(data);
-            // return data;
-            myApp.displayEnemy(data);
-            $('#enemy-img').attr('src', './assets/fishman2.png');
+            app.displayEnemy(data);
+            $('#enemy-img').attr('src', './assets/fishman2.png').attr('alt', 'Sahuagin');
             enemyHP = 60;
             $('#enemy-hp').append(`<p class="hp">HP: ${enemyHP}/60</p>`);
-            $('body').on('click', '#attack', function () {
+            $('body').on('click', '#attack', function() {
                 enemyHP = enemyHP - 5;
                 $('#enemy-hp').replaceWith(`<p class="hp" id="enemy-hp">HP: ${enemyHP}/60`);
-                console.log(enemyHP);
-                console.log($('#enemy-hp'));
-                myApp.checkWinner();
+                app.checkWinner();
             });
         });
         return enemyMonsterPromise;
@@ -265,114 +235,108 @@ myApp.getRandomMonster = () => {
                 reqUrl: `http://dnd5eapi.co/api/monsters/gelatinous-cube`,
             }
         }).then((data) => {
-            console.log(data);
-            // return data;
-            myApp.displayEnemy(data);
-            $('#enemy-img').attr('src', './assets/cube2.png');
+            app.displayEnemy(data);
+            $('#enemy-img').attr('src', './assets/cube2.png').attr('alt', 'Gelatinous Cube');
             enemyHP = 84;
             $('#enemy-hp').append(`<p class="hp">HP: ${enemyHP}/84</p>`);
-            $('body').on('click', '#attack', function () {
+            $('body').on('click', '#attack', function() {
                 enemyHP = enemyHP - 5;
                 $('#enemy-hp').replaceWith(`<p class="hp" id="enemy-hp">HP: ${enemyHP}/84`);
-                console.log(enemyHP);
-                console.log($('#enemy-hp'));
-                myApp.checkWinner();
+                app.checkWinner();
             });
         });
         return enemyMonsterPromise;
     }
 }
 
+
+// Decreasing monster HP during battle
+
 let enemyHP = 0;
 let userHP = 0;
 
-myApp.init = () => {
-   
-    $('body').one('click', '#owlbear', function(e) {
-        myApp.getOwlbear();
-        $('#monster-img').attr('src', './assets/Owlbear2.png');
+// Init 
+
+app.init = () => {
+    $('body').one('click', '#owlbear', function() {
+        app.getOwlbear();
+        $('#monster-img').attr('src', './assets/Owlbear2.png').attr('alt', 'Owlbear');
         userHP = 59;
         $('#monster-hp').append(`<p class="hp">HP: ${userHP}/59</p>`);
-        $('body').on('click', '#attack', function () {
+        $('body').on('click', '#attack', function() {
             userHP = userHP - 3;
             $('#monster-hp').replaceWith(`<p class="hp" id="monster-hp">HP: ${userHP}/59</p>`);
         });
     });
     $('body').on('click', '#goblin', function() {
-        myApp.getGoblin();
-        $('#monster-img').attr('src', './assets/gobbo.png');
+        app.getGoblin();
+        $('#monster-img').attr('src', './assets/gobbo.png').attr('alt', 'Goblin');
         userHP = 40;
-        $('#monster-hp').append(`<p class="hp">HP: ${userHP}/40</p>`)
-        $('body').on('click', '#attack', function () {
+        $('#monster-hp').append(`<p class="hp">HP: ${userHP}/40</p>`);
+        $('body').on('click', '#attack', function() {
             userHP = userHP - 3;
-            $('#monster-hp').replaceWith(`<p class="hp" id="monster-hp">HP: ${userHP}/40</p>`)
+            $('#monster-hp').replaceWith(`<p class="hp" id="monster-hp">HP: ${userHP}/40</p>`);
         });
     });
     $('body').on('click', '#orc', function() {
-        myApp.getOrc();
-        $('#monster-img').attr('src', './assets/orc.png');
+        app.getOrc();
+        $('#monster-img').attr('src', './assets/orc.png').attr('alt', 'Orc');
         userHP = 50;
         $('#monster-hp').append(`<p class="hp">HP: ${userHP}/50</p>`);
-        $('body').on('click', '#attack', function () {
+        $('body').on('click', '#attack', function() {
             userHP = userHP - 3;
             $('#monster-hp').replaceWith(`<p class="hp" id="monster-hp">HP: ${userHP}/50</p>`);
         });
     });
 
     $('body').on('click', '#sahuagin', function() {
-        myApp.getSahuagin();
-        $('#monster-img').attr('src', './assets/fishman2.png');
+        app.getSahuagin();
+        $('#monster-img').attr('src', './assets/fishman2.png').attr('alt', 'Sahuagin');
         userHP = 60;
-        $('#monster-hp').append(`<p class="hp">HP: ${userHP}/60</p>`)
-        $('body').on('click', '#attack', function () {
+        $('#monster-hp').append(`<p class="hp">HP: ${userHP}/60</p>`);
+        $('body').on('click', '#attack', function() {
             userHP = userHP - 3;
             $('#monster-hp').replaceWith(`<p class="hp" id="monster-hp">HP: ${userHP}/60</p>`);
         });
     });
 
     $('body').on('click', '#troll', function() {
-        myApp.getTroll();
-        $('#monster-img').attr('src', './assets/O-Mad-Troll-Monster-620-Wide-with-margins.png');
+        app.getTroll();
+        $('#monster-img').attr('src', './assets/O-Mad-Troll-Monster-620-Wide-with-margins.png').attr('alt', 'Troll');
         userHP = 84;
         $('#monster-hp').append(`<p class="hp">HP: ${userHP}/84</p>`);
-        $('body').on('click', '#attack', function () {
+        $('body').on('click', '#attack', function() {
             userHP = userHP - 3;
             $('#monster-hp').replaceWith(`<p class="hp" id="monster-hp">HP: ${userHP}/84</p>`);
         });
     });
 
     $('body').on('click', '#gelatinous-cube', function() {
-        myApp.getGelatinousCube();
-        $('#monster-img').attr('src', './assets/cube2.png');
+        app.getGelatinousCube();
+        $('#monster-img').attr('src', './assets/cube2.png').attr('alt', 'Gelatinous Cube');
         userHP = 84;
         $('#monster-hp').append(`<p class="hp">HP: ${userHP}/84</p>`);
-        $('body').on('click', '#attack', function () {
+        $('body').on('click', '#attack', function() {
             userHP = userHP - 3;
             $('#monster-hp').replaceWith(`<p class="hp" id="monster-hp">HP: ${userHP}/84</p>`);
         });
-        
     });
 
-    $('body').on('click', '.monster', function () {
+
+    // Transition from gallery to battle page 
+
+    $('body').on('click', '.monster', function() {
         $('#attack-box').append('<button class="attack-button" id="attack">ATTACK!!!!</button>');
-        myApp.getRandomMonster();
+        app.getRandomMonster();
         $('.monster').attr("disabled", true);
         $('#gallery').hide();
     })
-
-
- 
-
-
-    // $('body').on('click', '#attack', function(){
-    //     userHP = userHP - 3;
-        
-        console.log(userHP)
-    // })
-
 }
 
-myApp.checkWinner=function(){
+
+// Checking for the winner of the battle
+
+app.checkWinner = function() {
     if (userHP < 1 && enemyHP < 1) {
         $('.body-container').replaceWith(`<section class='end-page'><h1>you tied</h1><p>Due to unfortunate circumstances, our two brave competitors have met with a grisly end. A sorry sight, our poor groundskeepers have a large mess to clean up.</p><a href=#><button id="try-again">try again</button></a></section>`);
     } else if (userHP < 1) {
@@ -381,25 +345,20 @@ myApp.checkWinner=function(){
         $('.body-container').replaceWith(`<section class='end-page'><h1>you win</h1><p>You live to see another day, but should you wish to tempt fate again, you know where to go!</p><a href=#><button id="try-again">try again</button></a></section>`);
     }
 
+
+    // Try again button brings you back to selection screen
+
     $('#try-again').on('click', function(){
         location.reload();
     });
 };
 
+
+// Document ready 
+
 $(function() {
-    myApp.init();
+    app.init();
 });
-// If (UserHP === 0) -> page says "You Lose"
-// If (OpponentHP === 0) -> page says "You Win"
-
-//STRETCH GOALS
-// If Strength > 15, then take off 10HP from opponent
-// If Strength < 15, then take off 5HP from opponent 
-
-// If Opponent Strength > 15, then take off 10HP from User
-// If Opponent Strength < 15, then take off 5HP from User
-
-//have a to-hit modifier. Attack === 0-1 = miss. attack === 2-9 = hit.
 
 
 
